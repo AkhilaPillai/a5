af048fe484e64d2058bcccbe9d0b9cdd629454ed
AkhilaPillai <36732292+AkhilaPillai@users.noreply.github.com>
2018-02-26 17:56:57 +0530

    Create rev.cpp

---
+++ rev.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    char b[70];
+    cout<<"\nSpecify the String : ";
+    cin>>b;
+    cout<<"\nString Before Reversing : "<<b;
+    cout<<"\nString After Reversing : " <<strrev(b);
+    return 0;
+}
+ 
