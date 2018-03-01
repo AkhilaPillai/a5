7c20cd357fd7fd895d6bbe377f1a1201f5fcf110
AkhilaPillai <36732292+AkhilaPillai@users.noreply.github.com>
2018-03-01 17:18:33 +0530

    Create holiday.cpp

---
+++ holiday.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+char a[50];
+cout<<"Enter the string:\n";
+cin>>a;
+if(a[0]='S' || a[0]='s')
+{
+cout<<"Holiday";
+}
+else
+{
+cout<<"Not A Holiday";
+}
+return 0;
+}
